19053aaed2a0d63a827547e72b84e78b8bf96165
Samarinneo <54540423+Samarinneo@users.noreply.github.com>
2020-10-16 22:49:53 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,57 @@
+// $(function(){
+// 	$('menu-open').click(function(){
+// 		$('.menu').toggleClass('show-menu')
+// 	})
+// })
+ 
+
+// // console.log("hello world") 
+// const firstname='yur'
+// const lastname='samarin'
+// const work='designer'
+// const huylo=2
+// const cars=[]
+// const comp=[]
+
+// // console.log(firstname,lastname);
+// // // alert(firstname)
+
+
+
+// // function huy(pizda,siska){
+// // 	console.log(pizda);
+// // 	console.log(siska);
+// // }
+
+// // function sum(x,y){
+// // 	return x+y
+// // }
+// // console.log('result of sum', sum(2,4))
+
+// // huy(firstname,lastname)
+// // huy(huylo,lastname)
+
+// // console.log(masiv)
+// // alert(masiv)
+
+
+// console.log(comp,'comp')
+
+// comp.push('huy')
+// comp.push('huylo', '4len')
+// comp.push('huynya')
+// console.log(comp)
+// cars.push("suzuki")
+
+// cars.push("fiat")
+// console.log("array of", cars)
+
+// const name='yuri'
+// const personality='huylo'
+// console.log(name,personality)
+
+function opener(){
+	const menuha_2=document.getElementById("menuha_2");
+	menuha_2.classList.toggle("open")
+}
+ 
